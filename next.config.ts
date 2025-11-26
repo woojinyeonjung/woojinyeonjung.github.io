2323c0a0e953067557fc76b1e6dc094220699fbc
woojimmy <lwj6079@gmail.com>
2025-11-26 18:37:24 +0900

    Publish

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+    /* config options here */
+    reactCompiler: true,
+    output: "export",
+    basePath: "/woojinyeonjung.github.io",
 };
 
 export default nextConfig;
