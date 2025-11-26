7cee9a9d448400fc32d31ce336c11c8194f95585
woojimmy <lwj6079@gmail.com>
2025-11-26 18:51:48 +0900

    Config

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,20 @@ const nextConfig: NextConfig = {
     reactCompiler: true,
     output: "export",
     basePath: "/woojinyeonjung.github.io",
+    assetPrefix: "/woojinyeonjung.github.io",
+    reactStrictMode: true,
+    trailingSlash: true,
+    async rewrites() {
+        return [
+            {
+                source: "/images/:path*",
+                destination: "/woojinyeonjung.github.io/images/:path*", // The :path parameter isn't used here so will be automatically passed in the query
+            },
+        ];
+    },
+    images: {
+        path: "/woojinyeonjung.github.io/_next/image",
+    },
 };
 
 export default nextConfig;
